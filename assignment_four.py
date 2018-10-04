1ecdd6c1ee1f34c619f6137e05c9ce50bf821930
Jonathan <jonathan@iMac9.local>
2018-10-04 12:46:59 -0400

    Roughly finished the program using if and else. Needed to revise and to put in comments in the future.

--- assignment_four.py
+++ assignment_four.py
@@ -5,16 +5,30 @@ def getCard():
     return card
 
 def getUserTotal():
-    user_first_total = getCard() + getCard()
-    print("Your total is ", user_first_total)
-    draw_a_card = input("Do you want to draw another card?")
+    user_total = getCard() + getCard()
+    print("Your total is ", user_total)
+    draw_a_card = input("Would you like to hit or stay?")
+    if draw_a_card == "hit":
+        user_total = user_total + getCard()
+        print("Your total is now", user_total)
+    return user_total
 
+def getDealerTotal():
+    dealer_total = getCard() + getCard()
+    print("The dealer total is", dealer_total)
+    return dealer_total
 
 
 def main():
-    userNumber = getUserTotal
-    dealerNumber = getDealerTotal
-    if userNumber => dealerNumber:
-        print("Congratulations, you win!")
-    if userNumber =< dealerNumber:
-        print("You lose.")
+    userNumber = getUserTotal()
+    if userNumber >= 21:
+        print("You lose")
+    else:
+        dealerNumber = getDealerTotal()
+        if userNumber >= dealerNumber:
+            print("You win")
+        else:
+            print("You lose")
+
+main()
+
